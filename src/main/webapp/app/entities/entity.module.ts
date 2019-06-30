840877bb4f19d237621f168de90d51d5640fc184
Mesqualito <jochen@gebsattel.rocks>
2019-06-30 21:08:07 +0200

    Initial application generated by JHipster-6.1.2

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,46 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'product',
+        loadChildren: './product/product.module#StoreProductModule'
+      },
+      {
+        path: 'shop-image',
+        loadChildren: './shop-image/shop-image.module#StoreShopImageModule'
+      },
+      {
+        path: 'product-reference',
+        loadChildren: './product-reference/product-reference.module#StoreProductReferenceModule'
+      },
+      {
+        path: 'product-substitution',
+        loadChildren: './product-substitution/product-substitution.module#StoreProductSubstitutionModule'
+      },
+      {
+        path: 'product-category',
+        loadChildren: './product-category/product-category.module#StoreProductCategoryModule'
+      },
+      {
+        path: 'customer',
+        loadChildren: './customer/customer.module#StoreCustomerModule'
+      },
+      {
+        path: 'product-order',
+        loadChildren: './product-order/product-order.module#StoreProductOrderModule'
+      },
+      {
+        path: 'order-item',
+        loadChildren: './order-item/order-item.module#StoreOrderItemModule'
+      },
+      {
+        path: 'invoice',
+        loadChildren: './invoice/invoice.module#StoreInvoiceModule'
+      },
+      {
+        path: 'shipment',
+        loadChildren: './shipment/shipment.module#StoreShipmentModule'
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ],
